b47b9e9a6e29e038cab94e3c1d3a859d3907f7c7
Liam <leemthompo@gmail.com>
2021-07-13 18:15:07 +0200

    Break logic into logical functions.
    Created `deal`, `show_card`, `blind_moves` functions to separate logic.
    Also added `button_or_blind` function to toss a coin and determine which
    player is on the button first. This needs to be expanded, to alternate
    button and blind positions, and to choose suitable method for tagging
    players as being on the button or on the blind.

--- AKQ.py
+++ AKQ.py
@@ -45,33 +45,76 @@ big_ace = """
  │        A│
  └─────────┘"""
 
-## TODO betting logic
-
-def bet():
-  pass
+# TODO betting logic
 
 
-## TODO break down into logical functions and abstract certain logic aspects
+def ante():
+    """Take one chip from each players chipstack as the ante"""
+    pass
+
+
+def button_or_blind():
+    """Determines which player is on button and which is on blind.
+    The opening positions are determined by a coin-toss.
+    If the human wins the cointoss they go on button for first round."""
+    toss = random.randint(0, 1)
+    decision = input("Heads or Tails? (Respond H or T)")
+
+    print("You picked", decision)
+    print("Flipping coin...")
+    time.sleep(2)
+
+    if toss == 0:
+        print("Heads")
+        if decision.upper() == "H":
+            print("You won the cointoss. You are on the button in the first round")
+            button = 0
+            blind = 1
+        else:
+            print("You lost the cointoss. You are the blind in the first round")
+            button = 1
+            blind = 0
+    else:
+        print("Tails")
+        if decision.upper() == "T":
+            print("You won the cointoss. You are on the button in the first round")
+            button = 0
+            blind = 1
+        else:
+            print("You lost the cointoss. You are the blind in the first round")
+            button = 1
+            blind = 0
+
+    return button, blind
+
+
+def deal(deck):
+    """Deal the cards from the shuffled 3-card deck
+    and return each players hand."""
+
+    computer_hand = 0
+    your_hand = 0
+
+    shuffled_deck = random.sample(deck, len(deck))
+    blind_card = shuffled_deck[0]
+    button_card = shuffled_deck[1]
+
+    computer_hand += int(blind_card)
+    your_hand += int(button_card)
+
+    return computer_hand, your_hand
+
+# TODO break down into logical functions and abstract certain logic aspects
+
+
 def play(deck):
-    "Deal the cards from the shuffled 3-card deck"
     global computer_score
     global your_score
     global big_queen
     global big_king
     global big_ace
-    
-    computer_hand = 0
-    your_hand = 0
-
-    computer_move = 0
-    your_move = 0
-
-    shuffled_deck = random.sample(deck, len(deck))
-    first_card = shuffled_deck[0]
-    second_card = shuffled_deck[1]
-
-    computer_hand += int(first_card)
-    your_hand += int(second_card)
+    global your_hand
+    global computer_hand
 
     print("You are on the button")
     print("\n")
@@ -81,8 +124,10 @@ def play(deck):
     print("New Hand")
     print("--------")
     print("\n")
-    
-    # Show hand
+
+
+def show_card(your_hand):
+    """Print the hand to the console."""
     if your_hand == 0:
         print("Your card: ", big_queen)
     elif your_hand == 1:
@@ -91,9 +136,21 @@ def play(deck):
         print("Your card: ", big_ace)
     time.sleep(2)
 
-    # Moves for computer on the blind 
+# TODO Here you need to break out your_move into a separate function
+# and return that move to be assessed in showdown logic
+
+
+def blind_moves(computer_hand, your_hand):
+    """Move for computer on the blind."""
+
+    global computer_score
+    global your_score
+
+    computer_move = 0
+    # your_move = 0
+
     if computer_hand == 0:
-        i = random.randint(1, 3) # Randomize bluffing with queens 1/3 of the time 
+        i = random.randint(1, 3)  # Randomize bluffing with Q 1/3 of the time
         if i == 3:
             computer_move += 2
         else:
@@ -133,7 +190,7 @@ def play(deck):
         decision = input("Call or fold: ")
         print("\n")
         time.sleep(1)
-        
+
         ## Showdown logic
         if decision.upper() == ("CALL") and your_hand > computer_hand:
             your_score += 1
@@ -156,12 +213,19 @@ def play(deck):
 
 while your_score < 5 and computer_score < 5:
     play(deck)
+    medeal = deal(deck)
+    computer_hand = (medeal[0])
+    your_hand = (medeal[1])
+    show_card(your_hand)
+    blind_moves(computer_hand, your_hand)
+
     print("-------------------")
     print("Your score: ", your_score)
     print("Computer score: ", computer_score)
     print("-------------------")
     time.sleep(2)
     print("\n")
+
 else:
     if computer_score == 5:
         os.system("cowsay COMPUTER WINS!")
